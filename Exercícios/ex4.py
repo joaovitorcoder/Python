ec82bb8932b263af87e7425f581ebbc75de0fcd0
João Vitor <olhierturcojoaovitor@gmail.com>
2025-08-29 20:06:44 -0300

    Create ex4.py

---
+++ Exercícios/ex4.py
@@ -0,0 +1,6 @@
+Numero1 = int(input('Digite um valor: '))
+Numero2 = int(input('Digite outro valor: '))
+Soma = Numero1 + Numero2
+print('A soma entre {} e {} é: {}'.format(Numero1,Numero2,Soma))
+
+#Faça um programa que faça a soma de dois valores, e imprima o resultado
