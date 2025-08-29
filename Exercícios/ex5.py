4f3b56e9b7914b5e6f3ef6bbd7f2a64a6b630046
João Vitor <olhierturcojoaovitor@gmail.com>
2025-08-29 20:08:36 -0300

    comitando exercícios

---
+++ Exercícios/ex5.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um numero inteiro
+#e mostre seu sucessor e antecessor
+
+n = int(input('Digite um valor: '))
+print('Seu sucessor é:{}'.format(n+1))
+print('Seu antecessor é:{}'.format(n-1))
---
+++ Exercícios/ex6.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um numero e mostre o seu dobro, tripo e raiz quadrada
+
+n = int(input('Informe um número: '))
+print('Seu dobro é:{}'.format(n*2))
+print('Seu triplo é:{}'.format(n*3))
+print('Sua raiz quadrada é:{}'.format(n**(1/2)))
