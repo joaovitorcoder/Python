91a04704169ba307fccd05b28bf7551a1ee47b41
João Vitor <olhierturcojoaovitor@gmail.com>
2025-08-31 11:38:03 -0300

    Create ex30.py

---
+++ Exercícios/ex30.py
@@ -0,0 +1,6 @@
+#faça um programa que leia um numero inteiro qualquer e escreva se é par ou impar
+n = int(input('Digite um número inteiro: '))
+if n % 2 ==0:
+    print('O número é par!')
+else:
+    print('O número é impar!')
