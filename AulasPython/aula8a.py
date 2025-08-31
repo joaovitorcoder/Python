66e8ca5cf57f62a1d3f58d5684ebeb4e560c9c09
João Vitor <olhierturcojoaovitor@gmail.com>
2025-08-31 13:04:03 -0300

    Create aula8a.py

---
+++ AulasPython/aula8a.py
@@ -0,0 +1,11 @@
+#Código abaixo serve para calcular a raiz quadrada de um numero, mas usando um módulo importado
+
+# from math import sqrt
+# num = int(input('Digite um numero:'))
+# raiz = sqrt(num)
+# print('A raiz de {} é {:.2f}'.format(num, raiz))
+
+#Código abaixo serve para gerar um numero aleatório de 1 ate 10
+import random
+num = random.randint(1,10)
+print(num)
