8e5320f921c3885322841c47d0fc885ccc815075
João Vitor <olhierturcojoaovitor@gmail.com>
2025-08-31 11:38:54 -0300

    Create ex33.py

---
+++ Exercícios/ex33.py
@@ -0,0 +1,18 @@
+#faça um programa que leia 3 numeros e mostre qual é o maior e o menor entre eles
+a = float(input('Digite um número: '))
+b = float(input('Digite outro número: '))
+c = float(input('Digite outro número: '))
+#verificando quem e menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+#verificando quem é o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
