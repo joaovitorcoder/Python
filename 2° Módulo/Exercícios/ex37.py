32187dcb2d189a6818d0deddf4648e54eb1e6982
João Vitor <olhierturcojoaovitor@gmail.com>
2025-10-04 16:09:45 -0300

    Create ex37.py

---
+++ 2° Módulo/Exercícios/ex37.py
@@ -0,0 +1,16 @@
+#leia um numero inteiro q peça para o usuario escolher qual sera a base de conversao - 1 binario - 2 octal
+#3 hexadecimal
+
+num = int(input('Digite um número inteiro: '))
+
+escolha = int(input('Você deseja converter para \n 1 - binário \n 2 - octal \n 3 - hexadecimal \n'))
+
+if escolha == 1:
+    binario = bin(num)[2:]
+    print('Seu número em binário: {}'.format(binario))
+elif escolha == 2:
+    octal = oct(num)[2:]
+    print('Seu número em octal: {}'.format(octal))
+else:
+    hexadecimal = hex(num)
+    print('Seu número em hexadecimal: {}'.format(hexadecimal))
