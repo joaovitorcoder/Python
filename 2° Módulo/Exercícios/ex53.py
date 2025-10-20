d563772806b1bfa83bc2f2ef00d6953c90b9cd94
João Vitor <olhierturcojoaovitor@gmail.com>
2025-10-20 13:41:52 -0300

    Update ex53.py

--- 2° Módulo/Exercícios/ex53.py
+++ 2° Módulo/Exercícios/ex53.py
@@ -1,4 +1,13 @@
 #criar um programa que leia uma frase e diga se ela é um palindromo desconsiderando os espacos
 
-frase = input('Digite aqui uma frase: ')
+frase = input('Digite aqui uma frase: ').replace(' ', '').lower()
 
+invertida = ''
+
+for i in range(len(frase) - 1, -1 , -1):
+    invertida += frase[i]
+
+if invertida == frase:
+    print('É um palindromo')
+else:
+    print('Não é um palindromo')
