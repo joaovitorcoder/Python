ced927237b600dad9142bf4b3e842e88fbb188ea
João Vitor <olhierturcojoaovitor@gmail.com>
2025-11-17 16:39:52 -0300

    Update ex59.py

--- 2° Módulo/Exercícios/ex59.py
+++ 2° Módulo/Exercícios/ex59.py
@@ -1,22 +1,33 @@
 
 #criar um menu com somar,multiplicar, maior, novos numeros, sair do programa e o programa devera realizar
 #a operação solicitada
+maior = -99999999999999
+
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+soma = n1 + n2
+mult = n1 * n2
+if n1 > n2:
+    maior = n1
+else:
+    maior = n2
 
 
-contador = 0
-operacao = 1
-soma = 0
-mult = 0
-maior = -99999999999999999999999999999999
+operacao = 2
+
 while operacao != 0:
-    while contador <= 1:
-        n = float(input('Digite um numero: '))
+    operacao = int(input('Qual operação você deseja realizar\n[1]Somar\n[2]Multiplicar\n[3]Maior\n[4]Adicionar '
+                         'novos numeros\n[0]Fechar o programa: '))
+
+    if operacao == 1:
+        print('A soma é {}'.format(soma))
+    elif operacao == 2:
+        print('A multiplicacao é {}'.format(mult))
+    elif operacao == 3:
+        print('O maior numero é {}'.format(maior))
+    elif operacao == 4:
+        n = float(input('Digite outros numeros: '))
         soma += n
         mult *= n
         if n > maior:
             maior = n
-        contador += 1
-    operacao = int(input('Qual operação você deseja realizar\n[1]Somar\n[2]Multiplicar\n[3]Maior\n[4]Adicionar '
-                         'novos numeros\n[0]Fechar o programa: '))
-    if operacao == 3:
-        print('O maior é {}'.format(maior))
