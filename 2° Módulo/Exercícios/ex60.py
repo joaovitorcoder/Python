97ac19dd2b47dad566271bcea4bb0776397e5502
João Vitor <olhierturcojoaovitor@gmail.com>
2025-11-19 13:28:46 -0300

    Update ex60.py

--- 2° Módulo/Exercícios/ex60.py
+++ 2° Módulo/Exercícios/ex60.py
@@ -2,8 +2,15 @@
 
 num = int(input('Digite um numero: '))
 fatorial = 1
-temp = num  
+temp = num
 while temp > 0:
     fatorial *= temp
     temp -= 1
 print('O fatorial do numero {} é {}'.format(num, fatorial))
+
+#Fazendo com o for ao inves do while
+# fatorial = 1
+# Calcularfatorial = num
+# for c in range(1, Calcularfatorial + 1):
+#     fatorial *= c
+# print('O fatorial do numero {} é {}'.format(num, fatorial))
