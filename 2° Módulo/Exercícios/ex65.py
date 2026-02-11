70f1489f056c5a1b1301d326f4d297759a2d58ed
João Vitor <olhierturcojoaovitor@gmail.com>
2026-02-11 15:44:16 -0300

    Update ex65.py

--- 2° Módulo/Exercícios/ex65.py
+++ 2° Módulo/Exercícios/ex65.py
@@ -1,5 +1,6 @@
 #fazer um programa que vai ler varios valores inteiros, vai imprimir a média entre eles, qual o maior, o menor
 #e vai perguntar ao usuario se ele quer continuar a digitar valores ou nao
+#O programa ira começar a perguntar se o usuario vai querer continuar a partir do 10° numero
 
 maior = -999999
 menor = 999999
@@ -14,7 +15,7 @@ while endPoint == 'S':
     if n < menor:
         menor = n
     media = soma / vezesRodadas
-    if vezesRodadas == 10:
+    if vezesRodadas >= 10:
         endPoint = input('Voce deseja continuar a rodar o programa [S/N]: ').upper()
 
 print('A média entre todos os valores digitados foi de {}\nO menor foi valor foi {}\n'
