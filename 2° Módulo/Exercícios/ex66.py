5ed1326d2f999b0f48be2e7b0ac93058ccb328fb
João Vitor <olhierturcojoaovitor@gmail.com>
2026-02-14 12:05:43 -0300

    att

--- 2° Módulo/Exercícios/ex66.py
+++ 2° Módulo/Exercícios/ex66.py
@@ -1,10 +1,9 @@
-endPoint = 0
+
 soma = numerosDigitados = 0
-while endPoint != 999:
+while True:
     numeros = int(input('Digite numero inteiro[CASO QUEIRA PARA DIGITE 999]: '))
     if numeros == 999:
-        endPoint = 999
-    else:
-        numerosDigitados += 1
-        soma += numeros
-print('A soma dos {} numeros é de {}'.format(numerosDigitados, soma))
+        break
+    numerosDigitados += 1
+    soma += numeros
+print(f'A soma dos {numerosDigitados} números é de {soma}')
