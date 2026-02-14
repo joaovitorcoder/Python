d02bd6cfeb700538a00175a20248e374487cec98
João Vitor <olhierturcojoaovitor@gmail.com>
2026-02-14 09:34:29 -0300

    comitando ex68

---
+++ 2° Módulo/Exercícios/ex68.py
@@ -0,0 +1,3 @@
+#um programa que jogue par ou impar com o pc, o jogo só sera interrompido quando o jogador perder, mostrando o total de vitorias
+#consecutivas que ele conquistou ao final da partida
+
