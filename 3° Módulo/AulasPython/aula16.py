287cde7875cd73f55fb250c2fa1c5529208aa959
João Vitor <olhierturcojoaovitor@gmail.com>
2026-02-15 10:32:28 -0300

    Create aula16.py

---
+++ 3° Módulo/AulasPython/aula16.py
@@ -0,0 +1,26 @@
+#lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+# Tuplas são imutáveis
+
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+
+#for i in range(0, len(lanche)):
+#    print(lanche[i])
+
+#vai mostrar alem da comida o indice da comida
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}')]
+
+# O sorted coloca em ordem a tupla
+#print(sorted(lanche))
+
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = a + b
+# print(c.count(5))
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+
+# Apaga a tupla pessoa
+del(pessoa)
+print(pessoa)
