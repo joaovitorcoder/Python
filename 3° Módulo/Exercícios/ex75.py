6ac505dbb0ffda8639dd578bd89b14ef52006829
João Vitor <olhierturcojoaovitor@gmail.com>
2026-03-31 17:58:10 -0300

    Update ex75.py

--- 3° Módulo/Exercícios/ex75.py
+++ 3° Módulo/Exercícios/ex75.py
@@ -1,36 +1,21 @@
 #vai ler quatro numeros vai mostrar os quatros, vai retonar quantos noves tem, em qual posição apareceu o primeiro 3 e em os numeros pares
 #digitados
 
-numeros = []
-contador = primeiro3 = 0
-for c in range(4):
-    numeros.append(int(input(f'Digite um valor: ')))
-tuplaValores = tuple(numeros)
-print(f'Os valores digitados foram: {tuplaValores}')
+numeros = (
+    int(input('Digite um valor: ')),
+    int(input('Digite outro valor: ')),
+    int(input('Digite outro valor: ')),
+    int(input('Digite o ultimo valor: ')),
+)
 
-numeros = []
-for pos, i in enumerate(tuplaValores):
-    if i == 9:
-        contador += 1
-    if i == 3 and primeiro3 == 0:
-        primeiro3 = pos + 1
-    if i % 2 == 0:
-        numeros.append(i)
-numerosPares = tuple(numeros)
-del(numeros)
-print('')
 
-if contador:
-    print(f'O numero 9 apareceu {contador} vezes')
+print('os valores pares digitados foram: ', end='')
+for j in numeros:
+    print(j, end=" ")
+
+print(f'\nTem {numeros.count(9)} numero nove') #retorna o tanto de numeros noves que tem na tupla
+
+if 3 in numeros:
+    print(f'O primeiro valor 3 apareceu na {numeros.index(3) + 1} posição')#retorna o index do primeiro numero 3 que apareceu na tupla
 else:
-    print('Não apareceu o valor nove')
-
-if primeiro3:
-    print(f'O Valor 3 apareceu na posicao {primeiro3}')
-else:
-    print('Não apareceu o valor 3')
-
-if numerosPares:
-    print(f'Os numeros pares foram {numerosPares}')
-else:
-    print('Não apareceu valores pares')
+    print('Nao tem valor 3 na lista')
