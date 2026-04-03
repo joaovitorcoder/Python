1862e7b1fb9ec95c23f3cb0a4072e16ec626d674
João Vitor <olhierturcojoaovitor@gmail.com>
2026-04-03 15:36:45 -0300

    Update ex78.py

--- 3° Módulo/Exercícios/ex78.py
+++ 3° Módulo/Exercícios/ex78.py
@@ -1,26 +1,38 @@
 #Vai ler 5 valores, vai guarda-los dentro de uma lista, vai mostrar o menor e o maior valor e a suas respectivas posições na lista
 
 valores = list()
+indiceMaior = list()
+indiceMenor = list()
 
 for c in range(5):
     valores.append(int(input(f'Digite um valor: ')))
 
 maior = menor = valores[0]
-indiceMaior = indiceMenor = 0
 
-print(f'Você digitou os valors: ', end='')
+print(f'Você digitou os valores: ', end='')
+
 
 for indice, valor in enumerate(valores):
     if maior < valor:
         maior = valor
-        indiceMaior = indice
+        indiceMaior = [indice]
+    elif maior == valor:
+        indiceMaior.append(indice)
+
 
     if menor > valor:
         menor = valor
-        indiceMenor = indice
+        indiceMenor = [indice]
+    elif menor == valor:
+        indiceMenor.append(indice)
 
     print(valor, end=' ')
 
 
-print(f'\nNa posição {indiceMaior} temos o maior valor {maior}')
-print(f'Na posição {indiceMenor} temos o menor valor {menor}')
+print(f'\nO maior valor digitado foi {maior} nas posições: ', end='')
+for indice in indiceMaior:
+    print(indice, end=' ')
+
+print(f'\nO menor valor digitado foi {menor} nas posições: ', end='')
+for indice in indiceMenor:
+    print(indice, end=' ')
