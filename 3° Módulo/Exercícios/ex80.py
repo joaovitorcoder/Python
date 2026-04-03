0dfae014d50f995d557070cb91e075bcf95bf4cf
João Vitor <olhierturcojoaovitor@gmail.com>
2026-04-03 18:54:48 -0300

    Create ex80.py

---
+++ 3° Módulo/Exercícios/ex80.py
@@ -0,0 +1,7 @@
+#Um programa que vai ler cinco valores numericos, e os cadastre em uma lista ja na posição correta de inserção, sem usar o sort
+# No final mostre a lista na tela em ordem
+
+valores = []
+
+for i in range(5):
+    valores.append(int(input('Digite um valor: ')))
