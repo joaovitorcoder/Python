5c652559ebed16a8834d484f79e4243123c7dc18
João Vitor <olhierturcojoaovitor@gmail.com>
2026-04-04 14:42:53 -0300

    Update ex83.py

--- 3° Módulo/Exercícios/ex83.py
+++ 3° Módulo/Exercícios/ex83.py
@@ -1,1 +1,19 @@
 #vai ler uma expressão qualquer que use parenteses, vai analisar a expressao e ver se ta com os parenteses abertos e fechados corretamente
+
+expressao = input('Digite uma expressão: ')
+pilha = []
+
+for caractere in expressao:
+    if caractere == '(':
+        pilha.append('(')
+    elif caractere == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha) == 0:
+    print('Expressão válida')
+else:
+    print('Expressão inválida')
