c658a408d85fe71e8e5dde8363368882a35d98dd
João Vitor <olhierturcojoaovitor@gmail.com>
2026-04-04 13:36:14 -0300

    Create ex82.py

---
+++ 3° Módulo/Exercícios/ex82.py
@@ -0,0 +1,32 @@
+# o programa vai ler varios numeros, joga los em uma lista, separar em mais duas listas os pares e impares, e depois mostrar
+numeros = []
+pares = []
+impares = []
+
+while True:
+    num = int(input('Digite um valor: '))
+    numeros.append(num)
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+    resp = input('Deseja continuar? [S/N]: ').strip().lower()
+    if resp == 'n':
+        break
+
+numeros.sort()
+pares.sort()
+impares.sort()
+
+print('-' * 30)
+print('Os valores digitados foram: ',end='')
+for n in numeros:
+    print(f'{n}', end=' ')
+
+print('\nOs valores pares foram: ',end='')
+for p in pares:
+    print(f'{p}', end=' ')
+
+print('\nOs valores impares foram: ', end='')
+for i in impares:
+    print(i, end=' ')
