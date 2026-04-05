c66afa8fc2b5a3c33ae8fc7df339efb25bf89b8d
João Vitor <olhierturcojoaovitor@gmail.com>
2026-04-05 09:54:39 -0300

    Update ex80.py

--- 3° Módulo/Exercícios/ex80.py
+++ 3° Módulo/Exercícios/ex80.py
@@ -1,7 +1,20 @@
 #Um programa que vai ler cinco valores numericos, e os cadastre em uma lista ja na posição correta de inserção, sem usar o sort
-# No final mostre a lista na tela em ordem
+# No final mostre a lista na tela em ordem, 5, 2, 8, 1, 3
 
 valores = []
+maior = menor = 0
 
 for i in range(5):
-    valores.append(int(input('Digite um valor: ')))
+    valor = int(input('Digite um valor: '))
+    if len(valores) == 0:
+        valores.append(valor)
+        print('Adicionando o valor ao final da lista...')
+        maior = valor
+    elif maior > valor:
+        print('Adicionando na posição 0 da lista...')
+        valores.insert(0, valor)
+        menor = valor
+    elif maior < valor:
+        print('Adicionando o valor ao final da lista...')
+        valores.append(valor)
+        maior = valor
