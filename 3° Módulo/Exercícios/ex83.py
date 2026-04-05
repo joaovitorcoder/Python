3c271f25cd02839f11052cd78adf4e3b8c27d387
João Vitor <olhierturcojoaovitor@gmail.com>
2026-04-05 08:46:50 -0300

    Update ex83.py

--- 3° Módulo/Exercícios/ex83.py
+++ 3° Módulo/Exercícios/ex83.py
@@ -1,19 +1,23 @@
 #vai ler uma expressão qualquer que use parenteses, vai analisar a expressao e ver se ta com os parenteses abertos e fechados corretamente
 
-expressao = input('Digite uma expressão: ')
-pilha = []
+while True:
+    expressao = input('Digite uma expressão: ')
+    pilha = []
 
-for caractere in expressao:
-    if caractere == '(':
-        pilha.append('(')
-    elif caractere == ')':
-        if len(pilha) > 0:
-            pilha.pop()
-        else:
-            pilha.append(')')
-            break
+    for caractere in expressao:
+        if caractere == '(':
+            pilha.append('(')
+        elif caractere == ')':
+            if len(pilha) > 0:
+                pilha.pop()
+            else:
+                pilha.append(')')
 
-if len(pilha) == 0:
-    print('Expressão válida')
-else:
-    print('Expressão inválida')
+    if len(pilha) == 0:
+        print('Expressão válida')
+    else:
+        print('Expressão inválida')
+
+    resp = input('Deseja verificar outra expressão? [S/N]: ').strip().lower()[0]
+    if resp == 'n':
+        break
