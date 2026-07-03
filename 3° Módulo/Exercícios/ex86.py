d89b0d60cab4025dc16639f466d232749418a2d8
João Vitor <olhierturcojoaovitor@gmail.com>
2026-07-03 12:38:04 -0300

    Update ex86.py

--- 3° Módulo/Exercícios/ex86.py
+++ 3° Módulo/Exercícios/ex86.py
@@ -1,16 +1,25 @@
+#vai criar uma matriz 3x3 e vai preencher com os valores lidos pelo teclado
+#no final vai mostrar a matriz na tela com a formatação correta
+
 matriz = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
           ]
 
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        matriz[i][j] = int(input(f'Digite um valor para [{i}, {j}]: '))
+for linha in range(len(matriz)): # percorre todas as linhas da matriz, no caso 0, 1 ,2
+    for coluna in range(len(matriz[linha])): # no valor da linha atual, percorre todas as colunas
+        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
 
 
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        print(f'[{matriz[i][j]}]', end='\n')
+# printando os valores da matriz
+
+print(f'{"-" * 30} valores da matriz {"-" * 30}')
+
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz[linha])):
+        print(f'[{matriz[linha][coluna]:^3}]', end='')
+    print()
+print('-' * 80)
 
 
